7a47537605e46bec89acda5c9a138169eb86f849
kevtolan <82405167+kevtolan@users.noreply.github.com>
2021-10-27 14:41:45 -0400

    Update Useful_Commands.R

--- Useful_Commands.R
+++ Useful_Commands.R
@@ -7,7 +7,7 @@ qry(db.path = db.path,
 RSQLite::dbReadTable(conn = conx,
                      name = 'recordings')
 
-# add column to table
+# add column
 RSQLite::dbExecute(conn = conx, 
                    statement = "ALTER TABLE people 
                                 ADD COLUMN startDate varchar;") 
@@ -20,5 +20,13 @@ RSQLite::dbExecute(conn = conx,
 # update records
 RSQLite::dbExecute(conn = conx, 
                    statement = "UPDATE recordings 
-                                SET speciesID = 'phai'
-                                WHERE commonName = 'Phainopepla' ")
+                                SET speciesID = 'wofr'
+                                WHERE commonName = 'Wood Frog' ")
+
+# export csv of scores
+scores <- dbGetQuery(conn = conx, 
+                     statement = "SELECT scoreID, recordingID, templateID, 
+                                         time, scoreThreshold, score, 
+                                         manualVerifyLibraryID, manualVerifySpeciesID
+                                  FROM scores")
+write.csv(scores,paste0(siteID,'_scores.csv'))
