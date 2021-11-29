441c36132ebdec754a7c95213ce54cf5d6158253
kevtolan <82405167+kevtolan@users.noreply.github.com>
2021-11-29 17:39:17 -0500

    Update Useful_Commands.R

--- Useful_Commands.R
+++ Useful_Commands.R
@@ -23,13 +23,32 @@ RSQLite::dbExecute(conn = conx,
                                 SET speciesID = 'wofr'
                                 WHERE commonName = 'Wood Frog' ")
 
-# export csv of scores
-scores <- dbGetQuery(conn = conx, 
-                     statement = "SELECT scoreID, recordingID, templateID, 
-                                         time, scoreThreshold, score, 
-                                         manualVerifyLibraryID, manualVerifySpeciesID
-                                  FROM scores")
-ggplot(scores) +
-  geom_point(aes(x = time, y= recordingID))
 
-write.csv(scores,paste0(siteID,'_2021_scores.csv'))
+# rename Olympus files. time <- '_20-00-00' SET TIME IN EST
+AudioFiles11 <- list.files(path = "recording_drop", pattern = ".WAV", all.files = TRUE,
+                         full.names = TRUE, recursive = TRUE,
+                         ignore.case = TRUE, include.dirs = TRUE)
+AudioFiles21 <- str_sub(AudioFiles11, end=-10)
+AudioFiles31 <- parse_date_time(AudioFiles21, "ymd", tz = 'EST')
+AudioFiles41 <- as.character(AudioFiles31)
+AudioFiles51 <- paste0(AudioFiles41,time)
+AudioFiles61 <- paste0(siteID,AudioFiles51)
+AudioFiles71 <- paste0(AudioFiles61,'.wav')
+
+
+
+
+
+# rename Audiomoth files ; 'yyyymmdd hhmmss.wav'
+AudioFiles <- list.files(path = "recording_drop", pattern = ".WAV", all.files = TRUE,
+                         full.names = TRUE, recursive = TRUE,
+                         ignore.case = TRUE, include.dirs = TRUE)
+AudioFiles2 <- parse_date_time(AudioFiles, "Ymd HMS", tz = 'UTC')
+AudioFiles3 <- as.character(AudioFiles2)
+AudioFiles4 <- str_replace_all(AudioFiles3,' ','_')
+AudioFiles5 <- str_replace_all(AudioFiles4,':','-')
+AudioFiles6 <- paste0('~',siteID,'_',AudioFiles5)
+AudioFiles7 <- str_replace_all(AudioFiles6,'~','recording_drop/')
+AudioFiles8 <- file.rename(AudioFiles,paste0(AudioFiles7,'.wav'))
+
+
