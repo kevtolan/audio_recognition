1ef13c01f2f5f348e08c128ad8b81fef26136b75
Kevin Tolan <82405167+kevtolan@users.noreply.github.com>
2024-08-31 15:30:23 -0400

    Update AMMv2.R

--- AMMv2.R
+++ AMMv2.R
@@ -30,6 +30,39 @@ db.path <- '~/AMMv2/database/dbExample.sqlite'
 conx <- RSQLite::dbConnect(drv = dbDriver('SQLite'), dbname = db.path)
 RSQLite::dbExecute(conn = conx, statement = "PRAGMA foreign_keys = ON;")
 
-
-
 AMMonitor::launchApp()
+
+mediafiles <- RSQLite::dbReadTable(conn = conx,
+                            name = 'media')
+mediafiles$Site  <- str_extract(mediafiles$filename, "[^_]+")
+mediafiles <- mediafiles[mediafiles$Site == "site1",]
+
+mediafiles$date <- paste0(mediafiles$start_date," ",mediafiles$start_time) %>%
+                              as.POSIXlt()
+
+DATESTART <- as.Date("2019-01-01")
+DATESTOP <- as.Date("2024-01-01")
+
+
+mediasubset <- mediafiles %>% filter(between(date, DATESTART, DATESTOP))
+
+Sys.time()
+start <- Sys.time()
+scores <- scoresDetect(
+  con = conx,
+  recordingNames = mediafiles$filename,
+  templateNames = "template_SDF791_20220418_150000_bin_AM1",
+  scoreThresholds = NA,
+  recordingRootPath = 'https://vpmon-audio.s3.amazonaws.com/',
+  ammlPath = paste0(getwd(), "/ammls"),
+  dbInsert = T,
+  showProgress = T
+)
+stop <- Sys.time()
+stop - start
+
+
+
+
+
+
