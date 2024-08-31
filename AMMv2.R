14c26e188fe628690067e1632d05fd2322703598
Kevin Tolan <82405167+kevtolan@users.noreply.github.com>
2024-08-31 15:26:21 -0400

    Update AMMv2.R

--- AMMv2.R
+++ AMMv2.R
@@ -1,1 +1,34 @@
+options(timeout=100)
+remotes::install_gitlab(
+  repo = "vtcfwru/ammonitor@AMMonitor2.0",
+  auth_token = Sys.getenv("GITLAB_PAT"),
+  host = "code.usgs.gov",
+  build_vignettes = FALSE,
+  dependencies = TRUE,
+  upgrade = "never")
 
+library(AMMonitor)
+library(aws.s3)
+library(tidyverse)
+library(RSQLite)
+library(DBI)
+# library(usethis)
+# edit_r_environ()
+
+setwd('~/R/AMMonitor_VPMon/VPMon_AMM')
+db.path <- '~/R/AMMonitor_VPMon/VPMon_AMM/database/VPMon_AMM.sqlite'
+conx <- RSQLite::dbConnect(drv = dbDriver('SQLite'), dbname = db.path)
+RSQLite::dbExecute(conn = conx, statement = "PRAGMA foreign_keys = ON;")
+
+my_filepath <- ammCreateDirectories(
+  amm_dirname = "dbExample",
+  filepath = getwd())
+
+dbCreate(
+  new_db_name = "dbExample.sqlite",
+  new_db_filepath = paste0(my_filepath, "/database"),
+  db_source = "default")
+
+
+
+AMMonitor::launchApp()
