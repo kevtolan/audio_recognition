785f898b3c9b805040cc397a404936e28393c88f
Kevin Tolan <82405167+kevtolan@users.noreply.github.com>
2024-09-10 14:35:30 -0400

    Update AMMv2.R

--- AMMv2.R
+++ AMMv2.R
@@ -63,6 +63,54 @@ stop - start
 
 
 
+### download AWS filelist
+bucketlist <- get_bucket_df(
+                    bucket = 'vpmon-audio',
+                    max = Inf)
+
+bucketlist <- bucketlist[- grep("Misc_Audio/", bucketlist$Key),]
+
+# bucketlist$Site <- substr(bucketlist$Key,0,10)
+
+bucketlist$Site  <- str_extract(bucketlist$Key, "[^_]+")
+bucketlist$filename  <- bucketlist$Key
+bucketlist$filepath <- paste0('https://vpmon-audio.s3.amazonaws.com/',bucketlist$filename)
+table(bucketlist$Site)
+
+
+bucketlist$Date <- str_sub(bucketlist$filename, start = -19, end = -5)
+
+buckettmp <- parse_date_time(bucketlist$Date, "Y-m-d_H-M-S", tz = "America/New_York")
+
+bucketlist$Date <- buckettmp
+bucketlist$start_date <- format(buckettmp,"%Y-%m-%d")
+bucketlist$start_time <- format(buckettmp, "%H:%M:%S")
+bucketlist$year <- format(buckettmp, "%Y")
+table(bucketlist$Site,bucketlist$year)
+
+bucketlist$Size <- as.numeric(bucketlist$Size)
+sum(bucketlist$Size)/1099511627776
+
+#add files to db
+
+
+bucketadd <- bucketlist[bucketlist$Site == "MLS619",]
+
+bucketadd <- bucketadd[,c("filename","filepath",'start_date','start_time')]
+
+bucketadd$pk_mediaid <- NA
+bucketadd$fk_visitid <- 8
+bucketadd$sb_exclude <- NA
+bucketadd$fk_sciencebaseid <- NA
+bucketadd$filesize <- NA
+bucketadd$timestamp <- NA
+bucketadd$media_type <- "audio"
+
+dbAppendTable(conx, "media", bucketadd)
+
+
+
+
 
 
 
